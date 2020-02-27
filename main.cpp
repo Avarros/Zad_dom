a4424dcb3539c78b59ed46c3e28980b2a20f4928
Avarros <60660200+Avarros@users.noreply.github.com>
2020-02-27 09:23:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,79 +4,79 @@ using namespace std;
 
 int main()
 {
-int aw/*kolumna w ktorej stoi wieza*/,bw/*wiersz wiezy*/,ap/*kolumna punktu*/,bp/*wiersz punktu*/;
-cout<<"Kolumna wiezy:"; cin>>aw;
-cout<<"Wiersz wiezy:"; cin>>bw;
-cout<<"Kolumna punktu:"; cin>>ap;
-cout<<"Wiersz punktu:"; cin>>bp;
+  int aw/*kolumna w ktorej stoi wieza*/,bw/*wiersz wiezy*/,ap/*kolumna punktu*/,bp/*wiersz punktu*/;
+  cout<<"Kolumna wiezy:"; cin>>aw;
+  cout<<"Wiersz wiezy:"; cin>>bw;
+  cout<<"Kolumna punktu:"; cin>>ap;
+  cout<<"Wiersz punktu:"; cin>>bp;
 
-if(aw<1 || aw>8 ||bw<1 || bw>8 ||ap<1 || ap>8 ||bp<1 || bp>8)
-{
-cout<<"Kolumna wiezy:"; cin>>aw;
-cout<<"Wiersz wiezy:"; cin>>bw;
-cout<<"Kolumna punktu:"; cin>>ap;
-cout<<"Wiersz punktu:"; cin>>bp;
-}
-if(aw!=ap && bw!=bp)
-{
-cout<<"NO";
-}
-else if(aw!=ap || bw!=bp)
-{
-if(aw!=ap)
-{
-if(bw>bp)
-{
-if(bw-bp==2)
-{
-cout<<"YES";
-}
-else
-{
-cout<<"NO";
-}
-}
-if(bw<bp)
-{
-if(bp-bw==2)
-{
-cout<<"YES";
-}
-else
-{
-cout<<"NO";
-}
-}
-}
-else if(bw!=bp)
-{
-if(aw>ap)
-{
-if(aw-ap==2)
-{
-cout<<"YES";
-}
-else
-{
-cout<<"NO";
-}
-}
-if(aw<ap)
-{
-if(ap-aw==2)
-{
-cout<<"YES";
-}
-else
-{
-cout<<"NO";
-}
-}
-}
-}
-if(aw==ap && bw==bp)
-{
-cout<<"YES;
-}
+  if(aw<1 || aw>8 ||bw<1 || bw>8 ||ap<1 || ap>8 ||bp<1 || bp>8)
+  {
+    cout<<"Kolumna wiezy:"; cin>>aw;
+    cout<<"Wiersz wiezy:"; cin>>bw;
+    cout<<"Kolumna punktu:"; cin>>ap;
+    cout<<"Wiersz punktu:"; cin>>bp;
+  }
+  if(aw!=ap && bw!=bp)
+  {
+    cout<<"NO";
+  }
+  else if(aw!=ap || bw!=bp)
+  {
+    if(aw!=ap)
+    {
+      if(bw>bp)
+      {
+        if(bw-bp==2)
+        {
+          cout<<"YES";
+        }
+        else
+        {
+          cout<<"NO";
+        }
+      }
+      if(bw<bp)
+      {
+        if(bp-bw==2)
+        {
+          cout<<"YES";
+        }
+        else
+        {
+          cout<<"NO";
+        }
+      }
+    }
+   else if(bw!=bp)
+   {
+    if(aw>ap)
+    {
+      if(aw-ap==2)
+      {
+        cout<<"YES";
+      }
+      else
+      {
+        cout<<"NO";
+      }
+    }
+    if(aw<ap)
+    {
+      if(ap-aw==2)
+      {
+        cout<<"YES";
+      }
+      else
+      {
+        cout<<"NO";
+      }
+    }
+    }
+  }
+  if(aw==ap && bw==bp)
+  {
+    cout<<"YES;
+  }
 return 0;
 }
