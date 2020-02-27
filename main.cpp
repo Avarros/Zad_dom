07a54be6ccadadfd75fa2f34a8acc09f7e5b12de
Avarros <60660200+Avarros@users.noreply.github.com>
2020-02-27 07:13:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,18 +5,74 @@ using namespace std;
 int main()
 {
     int aw/*kolumna w której stoi wieża*/,bw/*wiersz wieży*/,ap/*kolumna punktu*/,bp/*wiersz punktu*/;
-    cout<<"aw:"; cin>>aw;
-    cout<<"bw:"; cin>>bw;
-    cout<<"ap:"; cin>>ap;
-    cout<<"bp:"; cin>>bp;
+    cout<<"Kolumna wiezy:"; cin>>aw;
+    cout<<"Wiersz wiezy:"; cin>>bw;
+    cout<<"Kolumna punktu:"; cin>>ap;
+    cout<<"Wiersz punktu:"; cin>>bp;
 
     if(aw<1 || aw>8 ||bw<1 || bw>8 ||ap<1 || ap>8 ||bp<1 || bp>8)
     {
-        cout<<"aw:"; cin>>aw;
-        cout<<"bw:"; cin>>bw;
-        cout<<"ap:"; cin>>ap;
-        cout<<"bp:"; cin>>bp;
+        cout<<"Kolumna wiezy:"; cin>>aw;
+        cout<<"Wiersz wiezy:"; cin>>bw;
+        cout<<"Kolumna punktu:"; cin>>ap;
+        cout<<"Wiersz punktu:"; cin>>bp;
     }
-
+    if(aw!=ap && bw!=bp)
+            {
+	               cout<<"NO";
+            }
+     if(aw!=ap || bw!=bp)
+            {
+	               if(aw!=ap)
+	               {
+	               	if(bw>bp)
+	               	{
+	               		if(bw-bp==2)
+	               		{
+	               			cout<<"YES";
+	               		}
+	               		else
+	               		{
+	               			cout<<"NO";
+	               		}
+	               	}
+	               		if(bw<bp)
+	               	{
+	               		if(bp-bw==2)
+	               		{
+	               			cout<<"YES";
+	               		}
+	               		else
+	               		{
+	               			cout<<"NO";
+	               		}
+	               	}
+	               }
+	               if(bw!=bp)
+	               {
+	               	if(aw>ap)
+	               	{
+	               		if(aw-ap==2)
+	               		{
+	               			cout<<"YES";
+	               		}
+	               		else
+	               		{
+	               			cout<<"NO";
+	               		}
+	               	}
+	               		if(aw<ap)
+	               	{
+	               		if(ap-aw==2)
+	               		{
+	               			cout<<"YES";
+	               		}
+	               		else
+	               		{
+	               			cout<<"NO";
+	               		}
+	               	}
+	               }
+            }
     return 0;
 }
